37d85b16effc30de7137981b3a2954d00c5a5c8d
naong95 <sonic1@naver.com>
2020-07-08 22:39:17 +0900

    gamelevel

---
+++ Chapter8/src/gamelevel/Player.java
@@ -0,0 +1,25 @@
+package gamelevel;
+
+public class Player {
+	
+	private PlayerLevel level;
+	
+	public PlayerLevel getLevel() {
+		return level;
+	}
+
+	public Player() {
+		level = new BeginnerLevel();
+		level.showLevelMessage();
+	}
+	
+	public void upgradeLevel(PlayerLevel level) {
+		this.level = level;
+		level.showLevelMessage();
+	}
+	
+	public void play(int count) {
+		level.go(count);
+	}
+
+}
---
+++ Chapter8/src/gamelevel/SuperLevel.java
@@ -0,0 +1,24 @@
+package gamelevel;
+
+public class SuperLevel extends PlayerLevel {
+	
+	@Override
+	public void run() {
+		System.out.println("엄청 빠르게 달립니다.");
+	}
+
+	@Override
+	public void jump() {
+		System.out.println("매우 높이 점프 합니다.");
+	}
+
+	@Override
+	public void turn() {
+		System.out.println("턴 합니다.");
+	}
+
+	@Override
+	public void showLevelMessage() {
+		System.out.println("******* 고급자 레벨입니다. *******");
+	}
+}
