db644f65c400d725acf34f94129602214f803302
naong95 <sonic1@naver.com>
2020-10-04 22:16:09 +0900

    randomAccessFile

---
+++ Chapter14/src/stream/others/RandomAccessFileTest.java
@@ -0,0 +1,29 @@
+package stream.others;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileTest {
+
+	public static void main(String[] args) throws IOException {
+
+		RandomAccessFile rf = new RandomAccessFile("random.txt", "rw");
+		rf.writeInt(100);
+		System.out.println(rf.getFilePointer());
+		rf.writeDouble(3.14);
+		rf.writeUTF("안녕하세요");
+		
+		rf.seek(0); //초기 위치로
+		int i = rf.readInt();
+		double d = rf.readDouble();
+		String str = rf.readUTF();
+		
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println(str);
+		rf.close();
+		
+	}
+
+}
