c357923428fcd2108926b4535ccd9b232e4b4e41
naong95 <sonic1@naver.com>
2020-10-29 14:46:30 +0900

    dependency injection

--- eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,19 +1,19 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.Restaurant;
-import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 @RestController
 public class RestaurantController {
 
-    private RestaurantRepository repository = new RestaurantRepository();
+    @Autowired
+    private RestaurantRepositoryImpl repository;
 
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
@@ -22,7 +22,7 @@ public class RestaurantController {
         return restaurants;
     }
 
-    @GetMapping("/restaurant/{id}")
+    @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
         Restaurant restaurant = repository.findById(id);
 
--- eatgo/eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTests.java
+++ eatgo/eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTests.java
@@ -1,9 +1,11 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +22,9 @@ class RestaurantControllerTests {
     @Autowired
     private MockMvc mvc;
 
+    @SpyBean(RestaurantRepositoryImpl.class)
+    private RestaurantRepositoryImpl restaurantRepository;
+
     @Test
     public void list() throws Exception {
         mvc.perform(get("/restaurants"))
@@ -35,7 +40,7 @@ class RestaurantControllerTests {
 
     @Test
     public void detail() throws Exception {
-        mvc.perform(get("/restaurant/1004"))
+        mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":1004")
@@ -44,7 +49,7 @@ class RestaurantControllerTests {
                         containsString("\"name\":\"Bob zip\"")
                 ));
 
-        mvc.perform(get("/restaurant/2020"))
+        mvc.perform(get("/restaurants/2020"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":2020")
